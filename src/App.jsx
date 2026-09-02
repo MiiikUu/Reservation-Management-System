84860ad2f440e369cd82f402a1933e5fabd84dc5
MarlonVina <mjbvina@pcu.edu.ph>
2026-09-02 22:39:18 +0800

    files for the system

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Container from 'react-bootstrap/Container';
+
+function App() {
+  return (
+    <Container className="mt-5">
+      <h1>Reservation Management System</h1>
+    </Container>
+  );
+}
+
+export default App;
